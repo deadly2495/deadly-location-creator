851c6507d654dbd73b4ce8f6dfbc38ba8cacffd7
DEADLY PARASITE <60134703+devilson2495@users.noreply.github.com>
2024-03-18 01:19:59 +0530

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,25 @@
+Config = Config or {}
+
+---DEADLY-BLIPS  
+Config.blipsShow = true
+
+Config.Locations = {
+    [1] = {
+        vector = vector3(-474.45, -953.53, 23.69), 
+        text = "Construction Company", 
+        color = 60, 
+        sprite = 468, 
+        scale = 0.6,
+    },
+-- Example 
+-- [[
+-- [1] = {
+--     vector = vector3(0, 0, 0),
+--     text = "Blip Name", 
+--     color = 3, 
+--     sprite = Blip ID, 
+--     scale = size,
+-- }, ]]
+
+--- add more
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+author '⚓DEADLY PARASITE⚓'
+description 'deadlyparasitegaming - https://discord.gg/8j9hwbJxeK'
+version '1.0.0'
+scriptname 'deadly-blips'
+
+
+
+shared_script 'config.lua'
+client_script 'deadlyblips.lua'
